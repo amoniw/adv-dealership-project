f07af61a315b12e1a81111427195f7cb30018068
amoniw <walkeramoni@yahoo.com>
2024-05-21 23:54:38 -0400

    updated changes

--- src/main/java/com.pluralsight/ContractFileManager.java
+++ src/main/java/com.pluralsight/ContractFileManager.java
@@ -1,8 +1,29 @@
 package com.pluralsight;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ContractFileManager {
+    private String ContractFileManager;
+
+    public ContractFileManager(String contractFileManager) {
+        ContractFileManager = contractFileManager;
+        // add method to save contract by appending it to your contracts file
+        public class Main {
+            public static void main(String[] args) {
+            saleContract.saveToFile("contracts.txt");
+            leaseContract.saveToFile("contracts.txt");
+
+        }
+
+        }
 
 
 
 
-}
+
+        }
+
+
+
+    }
+
